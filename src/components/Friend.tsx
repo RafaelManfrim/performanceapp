a93c9c435f9b1a05d8783547bf81a45e25932cbb
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-09 14:28:30 -0300

    usando memo

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Text } from 'react-native';
 
 interface ItemProps {
   data: {
+    id: number;
     name: string;
     likes: number;
   }
 }
 
-export function Friend({ data }: ItemProps) {
+function FriendComponent({ data }: ItemProps) {
   return (
     <Text>{data.name} - Likes: {data.likes}</Text>
   );
 }
+
+// O componente friend só é renderizado se os novos props forem diferentes dos anteriores
+export const Friend = memo(FriendComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.data, nextProps.data);
+})
+
+// Um bom critério para usar o memo é usar em componentes puros, onde apenas são exibidos dados e não são manipulados.
+// Componentes muito grandes ou que renderizam muitas vezes também são bons candidatos ao memo
