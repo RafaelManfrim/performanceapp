605d91af0d682ea87f521931f2e4cd10ad70c315
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-09 14:42:43 -0300

    useMemo

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { Text, View } from 'react-native';
 import { Friend } from './Friend';
 
 interface ItemProps {
@@ -11,8 +11,20 @@ interface ItemProps {
 }
 
 export function FriendsList({ data }: ItemProps) {
+  // const totalLikes = data.reduce((likes, friend) => likes + friend.likes, 0);
+  // Por mais que o cálculo não é um calculo complexo, pela quantidade de dados ele acaba se tornando mais pesado e gerando lentidão.
+
+  const totalLikes = useMemo(() => {
+    return data.reduce((likes, friend) => likes + friend.likes, 0);
+  }, [data])
+  // O useMemo memoriza um valor
+  // Nesse caso, o totalLikes é calculado somente quando o data mudar.
+
+  // Ele é indicado caso um cálculo seja pesado e/ou possa repetir muitas vezes com o mesmo valor.
+
   return (
     <View>
+      <Text>Total de likes: {totalLikes}</Text>
       {data.map(friend => (
         <Friend key={String(friend.id)} data={friend} />
       ))}
